0a85f5ec1b9abe455d7bd643ce50a23531797fc3
Mykola Kushnir <mkushnir885@gmail.com>
2025-06-02 20:57:40 +0300

    chore: replace script for database waiting with `wait-on`
    REF: https://www.npmjs.com/package/wait-on

--- server/src/Services/GameService.Nodejs/wait-for-db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-until nc -z "$DATABASE_HOST" "$DATABASE_PORT" 2>/dev/null; do
-  sleep 1
-done
--- server/src/Services/LeaderboardService.Nodejs/wait-for-db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-until nc -z "$DB_HOST" "$DB_PORT" 2>/dev/null; do
-  sleep 1
-done
