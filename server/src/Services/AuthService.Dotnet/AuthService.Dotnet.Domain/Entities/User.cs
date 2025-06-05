6ff17d22a701e25990263b6cbb6a83fa683fc903
Nazg0r <gorchinshiy83@gmail.com>
2025-06-06 01:31:01 +0300

    feat: create user entity

---
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthService.Dotnet.Domain.Entities
+{
+	public class User
+	{
+		public int Id { get; set; } = default!;
+		public string Username { get; set; } = default!;
+		public string Email { get; set; } = default!;
+		public DateTime CreatedAt { get; set; } = default!;
+	}
+}
