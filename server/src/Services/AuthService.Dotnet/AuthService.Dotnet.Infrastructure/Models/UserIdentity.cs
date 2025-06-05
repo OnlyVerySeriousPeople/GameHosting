51a72b8f42fa07692595784a7249d23907f3d197
Nazg0r <gorchinshiy83@gmail.com>
2025-06-06 01:54:13 +0300

    feat: create UserIdentity model

---
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Infrastructure/Models/UserIdentity.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AuthService.Dotnet.Infrastructure.Models
+{
+	public class UserIdentity : IdentityUser
+	{
+		public string PlayerId { get; set; } = default!;
+		public DateTime CreatedAt { get; set; } = default!;
+
+	}
+}
