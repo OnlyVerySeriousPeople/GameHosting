75654a6759073bf682691ea7047156ee4eaa3ddd
Nazg0r <gorchinshiy83@gmail.com>
2025-06-06 14:48:22 +0300

    feat: add RefreshToken entity

---
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthService.Dotnet.Domain.Entities
+{
+	public class RefreshToken
+	{
+		public string Token { get; set; } = string.Empty;
+		public DateTime CreatedAt { get; set; } = default!;
+		public DateTime Expiration { get; set; } = default!;
+		public string UserId { get; set; } = string.Empty;
+	}
+}
