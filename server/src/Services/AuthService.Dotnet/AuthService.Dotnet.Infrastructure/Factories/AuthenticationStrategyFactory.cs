bc612df9f2c04922a053f9d42ce2b67126beaa4e
Nazg0r <gorchinshiy83@gmail.com>
2025-06-06 20:14:36 +0300

    feat: implement AuthenticationStrategyFactory

---
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Infrastructure/Factories/AuthenticationStrategyFactory.cs
@@ -0,0 +1,28 @@
+﻿using AuthService.Dotnet.Application.Contracts;
+using AuthService.Dotnet.Domain.Constants;
+using AuthService.Dotnet.Infrastructure.Strategies.AuthenticationStrategies;
+
+namespace AuthService.Dotnet.Infrastructure.Factories
+{
+	public class AuthenticationStrategyFactory(IServiceProvider serviceProvider) : IStrategyFactory<IAuthenticationStrategy>
+	{
+		private readonly Dictionary<string, Type> _strategies = new()
+		{
+			{ AuthServiceConstants.CredentialsProviderName, typeof(CredentialsAuthenticationStrategy) },
+			{ AuthServiceConstants.GoogleProviderName, typeof(GoogleAuthenticationStrategy) }
+		};
+		public IAuthenticationStrategy GetStrategy(string providerName)
+		{
+			if (!_strategies.TryGetValue(providerName.ToLower(), out var strategyType))
+				throw new NotSupportedException(
+					$"Authentication provider '{providerName}' is not supported.");
+
+			var service = serviceProvider.GetService(strategyType);
+			if (service is not IAuthenticationStrategy strategy)
+				throw new InvalidOperationException(
+					$"Type '{strategyType.Name}' is not registered in DI as IAuthenticationStrategy.");
+
+			return strategy;
+		}
+	}
+}
