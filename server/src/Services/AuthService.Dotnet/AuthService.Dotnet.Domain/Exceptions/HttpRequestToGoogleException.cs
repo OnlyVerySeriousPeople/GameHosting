3efcd3ddce09bc5eac9e6a14a5374a71368e2f64
Nazg0r <gorchinshiy83@gmail.com>
2025-06-06 18:27:35 +0300

    feat: create HttpRequestToGoogleException

---
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Domain/Exceptions/HttpRequestToGoogleException.cs
@@ -0,0 +1,7 @@
+﻿using System.Net;
+
+namespace AuthService.Dotnet.Exceptions
+{
+	public class HttpRequestToGoogleException(HttpStatusCode statusCode, string content)
+		: HttpRequestException($"Failed to get google services request. StatusCode: {statusCode}, Content: {content}");
+}
