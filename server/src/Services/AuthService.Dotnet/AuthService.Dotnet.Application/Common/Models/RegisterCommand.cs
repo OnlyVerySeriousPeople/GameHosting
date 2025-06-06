5f2f6f64ee207803aef09aac9b04cf90cb56ff9c
Nazg0r <gorchinshiy83@gmail.com>
2025-06-06 15:50:40 +0300

    feat: create register command/result models

---
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Application/Common/Models/RegisterCommand.cs
@@ -0,0 +1,7 @@
+﻿using AuthService.Dotnet.Application.Contracts;
+
+namespace AuthService.Dotnet.Application.Common.Models
+{
+	public record RegisterCommand(string Username, string Password, string Email, string PlayerId)
+		: ICommand<RegisterResult>;
+}
