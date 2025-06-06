554483b2ebb265e0ed1098d455770281694464cd
Nazg0r <gorchinshiy83@gmail.com>
2025-06-06 16:48:11 +0300

    feat: add AuthServiceConstants

---
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Domain/Constants/AuthServiceConstance.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthService.Dotnet.Domain.Constants
+{
+	public static class AuthServiceConstants
+	{
+		public const string CredentialsPrefix = "credentials:";
+		public const string GooglePrefix = "google:";
+		public const string CredentialsProviderName = "Credentials";
+		public const string GoogleProviderName = "Google";
+	}
+}
