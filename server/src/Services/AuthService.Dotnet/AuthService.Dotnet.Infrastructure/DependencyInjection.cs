c3b8f0e30b4f8ea2e9449b86cf5a1c23129949ee
Nazg0r <gorchinshiy83@gmail.com>
2025-06-06 15:13:54 +0300

    feat: configure Redis cashe

---
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using AuthService.Dotnet.Infrastructure.Extensions;
+using Microsoft.Extensions.Hosting;
+
+namespace AuthService.Dotnet.Infrastructure
+{
+	public static class DependencyInjection
+	{
+		public static IHostApplicationBuilder AddAuthInfrastructure(this IHostApplicationBuilder builder)
+		{
+			var config = builder.Configuration;
+
+			builder.Services.AddConfiguredExchangeRedisCache(config);
+			return builder;
+		}
+	}
+}
