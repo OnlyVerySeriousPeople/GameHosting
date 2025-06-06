bf098efd9bf5033826bf554746d7694a93749862
Nazg0r <gorchinshiy83@gmail.com>
2025-06-06 16:18:13 +0300

    feat: add UserCreationException

---
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Domain/Exceptions/UserCreationException.cs
@@ -0,0 +1,5 @@
+﻿namespace AuthService.Dotnet.Domain.Exceptions
+{
+	public class UserCreationException(string email, string? details = null) : Exception(
+		$"Failed to create user with email: {email}.{Environment.NewLine}{details}");
+}
