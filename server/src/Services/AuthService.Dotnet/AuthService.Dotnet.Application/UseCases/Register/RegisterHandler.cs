a37813a2d688e85d74ad443a3f9d86a2fdc46fbb
Nazg0r <gorchinshiy83@gmail.com>
2025-06-06 17:00:01 +0300

    feat: implement RegisterHandler

---
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Application/UseCases/Register/RegisterHandler.cs
@@ -0,0 +1,43 @@
+﻿using AuthService.Dotnet.Application.Common.Models;
+using AuthService.Dotnet.Application.Contracts;
+using AuthService.Dotnet.Domain.Constants;
+using AuthService.Dotnet.Domain.Entities;
+using AuthService.Dotnet.Domain.Exceptions;
+
+namespace AuthService.Dotnet.Application.UseCases.Register
+{
+	public class RegisterHandler(
+		IAuthHelper authHelper,
+		ITokenService tokenService)
+		: ICommandHandler<RegisterCommand, RegisterResult>
+	{
+		public async Task<RegisterResult> Handle(RegisterCommand command, CancellationToken cancellationToken)
+		{
+			var newUser = await authHelper.CreateNewUserAsync(
+				command.Email, command.Username, command.PlayerId, command.Password);
+
+			var (jwtToken, jwtExpirationDate) = tokenService.GenerateJwtToken(newUser);
+			var (refreshToken, refreshExpirationDate) = tokenService.GenerateRefreshToken();
+
+			var refreshTokenEntity = new RefreshToken
+			{
+				Token = refreshToken,
+				CreatedAt = DateTime.UtcNow,
+				Expiration = refreshExpirationDate,
+				UserId = newUser.Id
+			};
+
+			var isTokenStored = await tokenService.StoreRefreshTokenAsync(refreshTokenEntity,
+				AuthServiceConstants.CredentialsPrefix, cancellationToken);
+
+			if (!isTokenStored)
+				throw new StoreRefreshTokenException(newUser.Id);
+
+			return new RegisterResult(
+				JwtToken: jwtToken,
+				JwtExpiry: jwtExpirationDate,
+				RefreshToken: refreshToken,
+				RefreshExpiry: refreshExpirationDate);
+		}
+	}
+}
