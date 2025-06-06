65ebbc70ca1146e738a0556397c1dbf1f4bdcc49
Nazg0r <gorchinshiy83@gmail.com>
2025-06-06 20:18:38 +0300

    feat: add command/response login models

---
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Application/Common/Models/LoginCommand.cs
@@ -0,0 +1,12 @@
+﻿
+using AuthService.Dotnet.Application.Contracts;
+
+namespace AuthService.Dotnet.Application.Common.Models
+{
+	public record LoginCommand(
+		string? Email,
+		string? Password,
+		string? Code,
+		string Provider)
+		: ICommand<LoginResult>;
+}
---
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Application/Common/Models/LoginResult.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthService.Dotnet.Application.Common.Models
+{
+	public record LoginResult(
+		string JwtToken,
+		DateTime JwtExpiry,
+		string RefreshToken,
+		DateTime RefreshExpiry);
+}
