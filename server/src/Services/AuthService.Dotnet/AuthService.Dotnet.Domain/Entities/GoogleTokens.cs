8832efbfdaa669d0859613edd383663c3425b450
Nazg0r <gorchinshiy83@gmail.com>
2025-06-06 18:19:11 +0300

    feat: create GoogleTokens entity

---
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Domain/Entities/GoogleTokens.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace AuthService.Dotnet.Domain.Entities
+{
+	internal class GoogleTokens
+	{
+		[JsonProperty("access_token")] public string AccessToken { get; set; } = string.Empty;
+		[JsonProperty("expires_in")] public long ExpiresIn { get; set; }
+		[JsonProperty("refresh_token")] public string RefreshToken { get; set; } = string.Empty;
+		[JsonProperty("refresh_token_expires_in")] public long RefreshExpiresIn { get; set; }
+		[JsonProperty("scope")] public string Scope { get; set; } = string.Empty;
+		[JsonProperty("token_type")] public string TokenType { get; set; } = string.Empty;
+	}
+}
