76048bd45301d5197536b34f0e9759909b235757
Nazg0r <gorchinshiy83@gmail.com>
2025-06-06 12:45:08 +0300

    feat: create User-Identity mapping extensions

--- server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Infrastructure/Common/Mappings/MappingExtensions.cs
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Infrastructure/Common/Mappings/MappingExtensions.cs
@@ -1,6 +1,26 @@
-﻿namespace AuthService.Dotnet.Infrastructure.Common.Mappings
+﻿using AuthService.Dotnet.Domain.Entities;
+using AuthService.Dotnet.Infrastructure.Models;
+
+namespace AuthService.Dotnet.Infrastructure.Common.Mappings
 {
-	public class MappingExtensions
+	public static class MappingExtensions
 	{
+		public static UserIdentity ToIdentity(this User user) => new UserIdentity
+		{
+			Id = user.Id,
+			PlayerId = user.PlayerId,
+			UserName = user.Username,
+			Email = user.Email,
+			CreatedAt = user.CreatedAt
+		};
+
+		public static User ToDomain(this UserIdentity userIdentity) => new User
+		{
+			Id = userIdentity.Id,
+			PlayerId = userIdentity.PlayerId,
+			Username = userIdentity.UserName!,
+			Email = userIdentity.Email!,
+			CreatedAt = userIdentity.CreatedAt
+		};
 	}
 }
