b33a9656bd97cbc6dc7a291280ebfcb6023b283a
Nazg0r <gorchinshiy83@gmail.com>
2025-06-06 22:41:17 +0300

    feat: add command/result refreshToken models

---
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Application/Common/Models/RefreshTokenCommand.cs
@@ -0,0 +1,6 @@
+﻿using AuthService.Dotnet.Application.Contracts;
+
+namespace AuthService.Dotnet.Application.Common.Models
+{
+	public record RefreshTokenCommand(string Token, string Prefix) : ICommand<RefreshTokenResult>;
+}
