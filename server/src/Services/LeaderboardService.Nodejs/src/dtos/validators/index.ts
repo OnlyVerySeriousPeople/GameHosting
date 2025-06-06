598300f4a64fa1f8a1e02e46cde64307a1d0f35e
Mykola Kushnir <mkushnir885@gmail.com>
2025-06-06 13:57:34 +0300

    fix(leaderboard): rewrite broken decorator

--- server/src/Services/LeaderboardService.Nodejs/src/dtos/validators/index.ts
+++ server/src/Services/LeaderboardService.Nodejs/src/dtos/validators/index.ts
@@ -2,15 +2,15 @@ import {Data, Validator} from './types';
 import {isPlainObj, plainObj} from './plain_obj';
 import {RequestError} from '../../errors';
 import {greaterThanZero} from './greater_than_zero';
+import {methodDecoratorFactory} from '../../utils/method_decorator_factory';
 import {nonEmptyStr} from './non_empty_str';
-import {paramDecoratorFactory} from '../../utils/param_decorator_factory';
 
-const Check = <T extends Data>(...rules: Validator<T>[]) =>
-  paramDecoratorFactory(data => {
-    if (!isPlainObj(data)) {
+const CheckReq = <T extends Data>(...rules: Validator<T>[]) =>
+  methodDecoratorFactory(req => {
+    if (!isPlainObj(req)) {
       throw new RequestError('input data must be a plain object');
     }
-    for (const rule of rules) rule(data as T);
+    for (const rule of rules) rule(req as T);
   });
 
-export {Check, greaterThanZero, nonEmptyStr, plainObj};
+export {CheckReq, greaterThanZero, nonEmptyStr, plainObj};
--- server/src/Services/LeaderboardService.Nodejs/src/services/leaderboard_service.ts
+++ server/src/Services/LeaderboardService.Nodejs/src/services/leaderboard_service.ts
@@ -1,6 +1,6 @@
 import * as pb from '../gen/proto/leaderboard/v1/leaderboard_pb';
 import {
-  Check,
+  CheckReq,
   greaterThanZero,
   nonEmptyStr,
   plainObj,
@@ -24,13 +24,15 @@ export class LeaderboardService {
 
   @LogEndpoint('GetLeaderboard')
   @HandleError('cannot get leaderboard of this game')
-  async getLeaderboard(
-    @Check<pb.GetLeaderboardRequest>(
-      nonEmptyStr('gameId'),
-      greaterThanZero('entryLimit'),
-    )
-    {gameId, entryLimit, scoreCursor}: pb.GetLeaderboardRequest,
-  ): Promise<pb.GetLeaderboardResponse> {
+  @CheckReq<pb.GetLeaderboardRequest>(
+    nonEmptyStr('gameId'),
+    greaterThanZero('entryLimit'),
+  )
+  async getLeaderboard({
+    gameId,
+    entryLimit,
+    scoreCursor,
+  }: pb.GetLeaderboardRequest): Promise<pb.GetLeaderboardResponse> {
     const {entries, nextScoreCursor} = await this.model.getLeaderboard(
       gameId,
       entryLimit,
@@ -45,10 +47,10 @@ export class LeaderboardService {
 
   @LogEndpoint('DeleteLeaderboard')
   @HandleError('cannot delete leaderboard of this game')
-  async deleteLeaderboard(
-    @Check<pb.DeleteLeaderboardRequest>(nonEmptyStr('gameId'))
-    {gameId}: pb.DeleteLeaderboardRequest,
-  ): Promise<pb.DeleteLeaderboardResponse> {
+  @CheckReq<pb.DeleteLeaderboardRequest>(nonEmptyStr('gameId'))
+  async deleteLeaderboard({
+    gameId,
+  }: pb.DeleteLeaderboardRequest): Promise<pb.DeleteLeaderboardResponse> {
     await this.model.deleteLeaderboard(gameId);
 
     return create(pb.DeleteLeaderboardResponseSchema, {ok: true});
@@ -56,13 +58,15 @@ export class LeaderboardService {
 
   @LogEndpoint('UpdatePlayerStats')
   @HandleError('cannot update stats of this player for this game')
-  async updatePlayerStats(
-    @Check<pb.UpdatePlayerStatsRequest>(
-      nonEmptyStr('gameId', 'playerId'),
-      plainObj('stats'),
-    )
-    {gameId, playerId, stats}: pb.UpdatePlayerStatsRequest,
-  ): Promise<pb.UpdatePlayerStatsResponse> {
+  @CheckReq<pb.UpdatePlayerStatsRequest>(
+    nonEmptyStr('gameId', 'playerId'),
+    plainObj('stats'),
+  )
+  async updatePlayerStats({
+    gameId,
+    playerId,
+    stats,
+  }: pb.UpdatePlayerStatsRequest): Promise<pb.UpdatePlayerStatsResponse> {
     await this.model.updatePlayerStats(
       gameId,
       playerId,
@@ -75,10 +79,11 @@ export class LeaderboardService {
 
   @LogEndpoint('GetPlayerStats')
   @HandleError('cannot get stats of this player for this game')
-  async getPlayerStats(
-    @Check<pb.GetPlayerStatsRequest>(nonEmptyStr('gameId', 'playerId'))
-    {gameId, playerId}: pb.GetPlayerStatsRequest,
-  ): Promise<pb.GetPlayerStatsResponse> {
+  @CheckReq<pb.GetPlayerStatsRequest>(nonEmptyStr('gameId', 'playerId'))
+  async getPlayerStats({
+    gameId,
+    playerId,
+  }: pb.GetPlayerStatsRequest): Promise<pb.GetPlayerStatsResponse> {
     const entry = await this.model.getPlayerStats(gameId, playerId);
 
     return create(pb.GetPlayerStatsResponseSchema, {
@@ -88,10 +93,10 @@ export class LeaderboardService {
 
   @LogEndpoint('GetAllPlayerStats')
   @HandleError('cannot get stats of this player')
-  async getAllPlayerStats(
-    @Check<pb.GetAllPlayerStatsRequest>(nonEmptyStr('playerId'))
-    {playerId}: pb.GetAllPlayerStatsRequest,
-  ): Promise<pb.GetAllPlayerStatsResponse> {
+  @CheckReq<pb.GetAllPlayerStatsRequest>(nonEmptyStr('playerId'))
+  async getAllPlayerStats({
+    playerId,
+  }: pb.GetAllPlayerStatsRequest): Promise<pb.GetAllPlayerStatsResponse> {
     const entries = await this.model.getAllPlayerStats(playerId);
 
     return create(pb.GetAllPlayerStatsResponseSchema, {
@@ -101,10 +106,10 @@ export class LeaderboardService {
 
   @LogEndpoint('DeleteAllPlayerStats')
   @HandleError('cannot detete stats of this player')
-  async deleteAllPlayerStats(
-    @Check<pb.DeleteAllPlayerStatsRequest>(nonEmptyStr('playerId'))
-    {playerId}: pb.DeleteAllPlayerStatsRequest,
-  ): Promise<pb.DeleteAllPlayerStatsResponse> {
+  @CheckReq<pb.DeleteAllPlayerStatsRequest>(nonEmptyStr('playerId'))
+  async deleteAllPlayerStats({
+    playerId,
+  }: pb.DeleteAllPlayerStatsRequest): Promise<pb.DeleteAllPlayerStatsResponse> {
     await this.model.deleteAllPlayerStats(playerId);
 
     return create(pb.DeleteAllPlayerStatsResponseSchema, {ok: true});
