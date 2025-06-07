aa5eedd2644edf779624c0159eb366469f915a75
Nazg0r <gorchinshiy83@gmail.com>
2025-06-07 13:14:21 +0300

    feat: create command model for RemoveUserHandler

---
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Application/Common/Models/RemoveUserCommand.cs
@@ -0,0 +1,8 @@
+﻿using AuthService.Dotnet.Application.Contracts;
+using MediatR;
+
+namespace AuthService.Dotnet.Application.Common.Models
+{
+	public record RemoveUserCommand(string UserId)
+		: ICommand<Unit>;
+}
