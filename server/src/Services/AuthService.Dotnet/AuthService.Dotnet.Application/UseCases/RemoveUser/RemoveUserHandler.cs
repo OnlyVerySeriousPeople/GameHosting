9ac0eb6e09977055cbc082745dc86b43b2b9755f
Nazg0r <gorchinshiy83@gmail.com>
2025-06-07 13:47:34 +0300

    feat: implement RemoveUserHandler

---
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Application/UseCases/RemoveUser/RemoveUserHandler.cs
@@ -0,0 +1,15 @@
+﻿using AuthService.Dotnet.Application.Common.Models;
+using AuthService.Dotnet.Application.Contracts;
+using MediatR;
+
+namespace AuthService.Dotnet.Application.UseCases.RemoveUser
+{
+	public class RemoveUserHandler(IAuthHelper authHelper) : ICommandHandler<RemoveUserCommand, Unit>
+	{
+		public async Task<Unit> Handle(RemoveUserCommand command, CancellationToken cancellationToken)
+		{
+			await authHelper.RemoveAllUserDataAsync(command.UserId);
+			return Unit.Value;
+		}
+	}
+}
