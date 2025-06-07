7ea3a9ccf2d93e269927ae62bb8e56234e757d9d
Nazg0r <gorchinshiy83@gmail.com>
2025-06-07 13:25:57 +0300

    feat: create UserDeletionException

---
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Domain/Exceptions/UserDeletionException.cs
@@ -0,0 +1,5 @@
+﻿namespace AuthService.Dotnet.Domain.Exceptions
+{
+	public class UserDeletionException(string userId, string? details = "")
+		: Exception($"Failed to delete user with Id: {userId}.{Environment.NewLine}{details}");
+}
