c6eca4cd5f91b9ec0686f02edd5bce887c1fab1e
Nazg0r <gorchinshiy83@gmail.com>
2025-06-07 13:59:08 +0300

    feat: implement DropUserRefreshTokensHandler

---
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Application/UseCases/DropUserRefreshTokens/DropUserRefreshTokensHandler.cs
@@ -0,0 +1,16 @@
+﻿using AuthService.Dotnet.Application.Common.Models;
+using AuthService.Dotnet.Application.Contracts;
+using MediatR;
+
+namespace AuthService.Dotnet.Application.UseCases.DropUserRefreshTokens
+{
+	public class DropUserRefreshTokensHandler(IAuthHelper authHelper)
+		: ICommandHandler<DropUserRefreshTokensCommand, Unit>
+	{
+		public async Task<Unit> Handle(DropUserRefreshTokensCommand request, CancellationToken cancellationToken)
+		{
+			await authHelper.DropAllUserRefreshTokensAsync(request.UserId, cancellationToken);
+			return Unit.Value;
+		}
+	}
+}
