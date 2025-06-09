ab8dacd6bcdbdde7d7a74c1cbb5dff23c256b606
Mykola Kushnir <mkushnir885@gmail.com>
2025-06-09 03:53:33 +0300

    feat(leaderboard): update proto definitions

--- server/src/Services/LeaderboardService.Nodejs/src/gen_proto.ts
+++ server/src/Services/LeaderboardService.Nodejs/src/gen_proto.ts
@@ -1,2 +1,5 @@
-export * from './gen/proto/leaderboard/v1/leaderboard_pb';
-export * from './gen/proto/leaderboard/v1/types_pb';
+export * from './gen/proto/leaderboard/v1/entry_pb';
+export * from './gen/proto/leaderboard/v1/leaderboard_entry_pb';
+export * from './gen/proto/leaderboard/v1/leaderboard_service_pb';
+export * from './gen/proto/leaderboard/v1/player_stats_entry_pb';
+export * from './gen/proto/leaderboard/v1/stats_pb';
