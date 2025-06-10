015c36355b9c9f5791bcf1c8acc5e53ebeb2a4bc
Nazg0r <gorchinshiy83@gmail.com>
2025-06-10 22:22:37 +0300

    feat: implement RemoveUserValidator

---
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Application/UseCases/RemoveUser/RemoveUserValidator.cs
@@ -0,0 +1,33 @@
+﻿using AuthService.Dotnet.Application.Common.Models;
+using AuthService.Dotnet.Application.Validation.Validators;
+using AuthService.Dotnet.Application.Validation;
+using AuthService.Dotnet.Application.Validation.Builder;
+using AuthService.Dotnet.Domain.Entities;
+using AuthService.Dotnet.Domain.Exceptions;
+using MediatR;
+
+namespace AuthService.Dotnet.Application.UseCases.RemoveUser
+{
+	public class RemoveUserValidator : BaseValidator<RemoveUserCommand>
+	{
+		public override Result<Unit> Validate(RemoveUserCommand command)
+		{
+			try
+			{
+				var builder = new ValidationChainBuilder<string>();
+				var userIdValidationBuilder = new UserIdValidationBuilder();
+				var userIdValidationChain = userIdValidationBuilder.Build(builder);
+				var userIdValidationResult = userIdValidationChain.Handle(command.UserId);
+
+				if (userIdValidationResult.IsSuccess)
+					return Result<Unit>.Success(Unit.Value);
+
+				return Result<Unit>.Failure(ValidationsErrors.ValidationFailed(userIdValidationResult.Error));
+			}
+			catch (Exception ex)
+			{
+				return Result<Unit>.Failure(ValidationsErrors.IncorrectValidationHandlerUsage(ex.Message));
+			}
+		}
+	}
+}
