6ea452c66260917cdd5217b33e1f8ab03b14b1c0
Nazg0r <gorchinshiy83@gmail.com>
2025-06-10 22:17:51 +0300

    feat: implement BaseValidator

---
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Application/Validation/BaseValidator.cs
@@ -0,0 +1,24 @@
+﻿using AuthService.Dotnet.Application.Contracts;
+using AuthService.Dotnet.Domain.Entities;
+using AuthService.Dotnet.Domain.Exceptions;
+using MediatR;
+
+namespace AuthService.Dotnet.Application.Validation
+{
+	public abstract class BaseValidator<T> : IValidator<T>
+	{
+		public abstract Result<Unit> Validate(T request);
+
+		protected Result<Unit> BuildResult(params (bool IsSuccess, string Error)[] validations)
+		{
+			if (validations.All(v => v.IsSuccess))
+				return Result<Unit>.Success(Unit.Value);
+
+			var errors = validations
+				.Where(v => !v.IsSuccess && !string.IsNullOrEmpty(v.Error))
+				.Select(v => v.Error);
+
+			return Result<Unit>.Failure(ValidationsErrors.ValidationFailed(string.Join("", errors)));
+		}
+	}
+}
