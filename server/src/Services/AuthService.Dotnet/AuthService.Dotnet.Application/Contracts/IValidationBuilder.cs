b794b021fa399c165a80cb3d394a2b94741e5f9c
Nazg0r <gorchinshiy83@gmail.com>
2025-06-10 22:16:41 +0300

    feat: create concrete validation builders

---
+++ server/src/Services/AuthService.Dotnet/AuthService.Dotnet.Application/Contracts/IValidationBuilder.cs
@@ -0,0 +1,9 @@
+﻿using AuthService.Dotnet.Application.Validation.Builder;
+
+namespace AuthService.Dotnet.Application.Contracts
+{
+	public interface IValidationBuilder<T>
+	{
+		public IValidationHandler<T> Build(ValidationChainBuilder<T> builder);
+	}
+}
