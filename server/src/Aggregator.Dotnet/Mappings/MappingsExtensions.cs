9e30888e010accc853176d2eac3f602d76c13930
Nazg0r <gorchinshiy83@gmail.com>
2025-06-12 19:07:46 +0300

    feat: create mapping extensions for auth request Dtos

---
+++ server/src/Aggregator.Dotnet/Mappings/MappingsExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Aggregator.Dotnet.Dtos;
+using AuthService.Dotnet.Grpc;
+
+namespace Aggregator.Dotnet.Mappings
+{
+	public static class MappingsExtensions
+	{
+		public static RegisterRequest ToGrpc(this RegisterRequestDto dto) =>
+			new RegisterRequest
+			{
+				Username = dto.Username,
+				Password = dto.Password,
+				Email = dto.Email,
+				PlayerId = Guid.NewGuid().ToString()
+			};
+
+		public static LoginRequest ToGrpc(this LoginRequestDto dto) =>
+		new LoginRequest
+		{
+			EmailPassword = new EmailPassword
+			{
+				Email = dto.Email,
+				Password = dto.Password
+			},
+			Provider = dto.Provider
+		};
+
+		public static RefreshTokenRequest ToGrpc(this RefreshTokenRequestDto dto) =>
+			new RefreshTokenRequest
+			{
+				Token = dto.Token,
+				Prefix = dto.Prefix
+			};
+	}
+}
