8552a05262821d14254f2f007fdfade65902501f
Nazg0r <gorchinshiy83@gmail.com>
2025-06-12 13:21:02 +0300

    feat: configure authService proto client

--- server/src/Aggregator.Dotnet/Program.cs
+++ server/src/Aggregator.Dotnet/Program.cs
@@ -1,4 +1,12 @@
+using static AuthService.Dotnet.Grpc.AuthService;
+
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
+builder.Services.AddGrpcClient<AuthServiceClient>(options =>
+{
+	options.Address = new Uri(config["GrpcSettings:AuthServiceUrl"] ?? string.Empty);
+});
 
 var app = builder.Build();
 
