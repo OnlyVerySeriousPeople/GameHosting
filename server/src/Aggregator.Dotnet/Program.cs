1d73ba4075b0eb6c5b7e64ada1c0fafee51e1bb4
Nazg0r <gorchinshiy83@gmail.com>
2025-06-12 19:10:37 +0300

    faet: include auth endpoints

--- server/src/Aggregator.Dotnet/Program.cs
+++ server/src/Aggregator.Dotnet/Program.cs
@@ -1,13 +1,16 @@
-using static AuthService.Dotnet.Grpc.AuthService;
+using Aggregator.Dotnet.Endpoints;
+using AuthClient = AuthService.Dotnet.Grpc.AuthService.AuthServiceClient;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 
-builder.Services.AddGrpcClient<AuthServiceClient>(options =>
+builder.Services.AddGrpcClient<AuthClient>(options =>
 {
 	options.Address = new Uri(config["GrpcSettings:AuthServiceUrl"] ?? string.Empty);
 });
 
 var app = builder.Build();
+
+app.MapAuthEndpoints();
 
 app.Run();
