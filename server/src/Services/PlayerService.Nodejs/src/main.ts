eae5fd9e6b86f37910a323fdfa6376be9638dbac
Maksym Yatsenko <saddas1rambler@gmail.com>
2025-06-14 10:41:35 +0300

    feat(player): add gRPC transport configuration to main.ts

--- server/src/Services/PlayerService.Nodejs/src/main.ts
+++ server/src/Services/PlayerService.Nodejs/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { PLAYERS_PACKAGE_NAME } from '@proto/types';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: PLAYERS_PACKAGE_NAME,
+      protoPath: join(process.cwd(), 'proto/player_service.proto'),
+      url: 'localhost:50051',
+    },
+  });
+
+  await app.startAllMicroservices();
 }
+
 bootstrap();
