3eee11779c9f97ea2878cb07e6bc8e53b3963f21
Nazg0r <gorchinshiy83@gmail.com>
2025-06-14 16:24:44 +0300

    feat: write gameServiceDtos mapping extensions

--- server/src/Aggregator.Dotnet/Mappings/MappingsExtensions.cs
+++ server/src/Aggregator.Dotnet/Mappings/MappingsExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using Aggregator.Dotnet.Dtos;
+using Aggregator.Dotnet.Dtos.GameService;
 using AuthService.Dotnet.Grpc;
+using Game.V1;
 
 namespace Aggregator.Dotnet.Mappings
 {
@@ -31,5 +33,33 @@ namespace Aggregator.Dotnet.Mappings
 				Token = dto.Token,
 				Prefix = dto.Prefix
 			};
+
+		public static GameMetadata ToGrpc(this GameMetadataDto dto) =>
+			new GameMetadata
+			{
+				Name = dto.Name,
+				Description = dto.Description,
+				Categories = { dto.Categories },
+				IconUrl = dto.IconUrl
+			};
+
+		public static CreateGameRequest ToGrpc(this CreateGameRequestDto dto) =>
+			new CreateGameRequest
+			{
+				AuthorId = dto.AuthorId,
+				Metadata = dto.Metadata.ToGrpc()
+			};
+
+		public static GetGamesRequest ToGrpc(this GetGamesRequestDto dto) =>
+			new GetGamesRequest
+			{
+				Name = dto.FiltersBy.Name,
+				UseCategories = dto.FiltersBy.UseCategories,
+				AuthorId = dto.FiltersBy.AuthorId,
+				Categories = { dto.Categories },
+				Offset = dto.Offset,
+				Limit = dto.Limit,
+			};
+
 	}
 }
