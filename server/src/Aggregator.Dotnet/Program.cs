1b25ec3b2dea64040ff566ad45317bb89e0c4bbf
Nazg0r <gorchinshiy83@gmail.com>
2025-06-16 13:09:46 +0300

    feat: include GemeEndpoints and GameClient

--- server/src/Aggregator.Dotnet/Program.cs
+++ server/src/Aggregator.Dotnet/Program.cs
@@ -1,5 +1,7 @@
 using Aggregator.Dotnet.Endpoints;
 using AuthClient = AuthService.Dotnet.Grpc.AuthService.AuthServiceClient;
+using GameClient = Game.V1.GameService.GameServiceClient;
+
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
@@ -9,8 +11,14 @@ builder.Services.AddGrpcClient<AuthClient>(options =>
 	options.Address = new Uri(config["GrpcSettings:AuthServiceUrl"] ?? string.Empty);
 });
 
+builder.Services.AddGrpcClient<GameClient>(options =>
+{
+	options.Address = new Uri(config["GrpcSettings:GameServiceUrl"] ?? string.Empty);
+});
+
 var app = builder.Build();
 
 app.MapAuthEndpoints();
+app.MapGameEndpoints();
 
 app.Run();
