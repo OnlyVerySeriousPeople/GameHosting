d65b2abc6c56a0cc2337b5ca8397a5b48f9b033f
Nazg0r <gorchinshiy83@gmail.com>
2025-06-16 15:23:55 +0300

    feat: create and include Leaderboard grpc client

--- server/src/Aggregator.Dotnet/Program.cs
+++ server/src/Aggregator.Dotnet/Program.cs
@@ -1,20 +1,10 @@
 using Aggregator.Dotnet.Endpoints;
-using AuthClient = AuthService.Dotnet.Grpc.AuthService.AuthServiceClient;
-using GameClient = Game.V1.GameService.GameServiceClient;
-
+using Aggregator.Dotnet.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 
-builder.Services.AddGrpcClient<AuthClient>(options =>
-{
-	options.Address = new Uri(config["GrpcSettings:AuthServiceUrl"] ?? string.Empty);
-});
-
-builder.Services.AddGrpcClient<GameClient>(options =>
-{
-	options.Address = new Uri(config["GrpcSettings:GameServiceUrl"] ?? string.Empty);
-});
+builder.Services.AddConfiguredGrpcClients(config);
 
 var app = builder.Build();
 
