b1b2e820d331b0443e33a23679cefb61d82675b9
Maksym Yatsenko <saddas1rambler@gmail.com>
2025-06-16 13:37:34 +0300

    fix(player): register PlayersRepository in providers

--- server/src/Services/PlayerService.Nodejs/src/modules/players/players.module.ts
+++ server/src/Services/PlayerService.Nodejs/src/modules/players/players.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PlayersService } from './players.service';
 import { PlayersController } from './players.controller';
 import { DatabaseModule } from '../../database/database.module';
+import { PlayersRepository } from './players.repository';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [PlayersController],
-  providers: [PlayersService],
+  providers: [PlayersService, PlayersRepository],
 })
 export class PlayersModule {}
