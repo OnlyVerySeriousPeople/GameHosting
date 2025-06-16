8d24b0220be866aaa1fff3e324c1454e16b19218
Maksym Yatsenko <saddas1rambler@gmail.com>
2025-06-16 23:47:55 +0300

    chore(player): import all modules within AppModule

--- server/src/Services/PlayerService.Nodejs/src/app.module.ts
+++ server/src/Services/PlayerService.Nodejs/src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { PlayersModule } from './modules/players/players.module';
+import { PlayerAchievementsModule } from './modules/player-achievements/player-achievements.module';
+import { AchievementsModule } from './modules/achievements/achievements.module';
+import { PlayerStatisticsModule } from './modules/player-statistics/player-statistics.module';
+import { FeaturedGamesModule } from './modules/featured-games/featured-games.module';
 
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    PlayersModule,
+    PlayerStatisticsModule,
+    PlayerAchievementsModule,
+    AchievementsModule,
+    FeaturedGamesModule,
+  ],
   controllers: [],
   providers: [],
 })
