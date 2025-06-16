ad09cd841de170255efe7b61434a53f38514d0d7
Maksym Yatsenko <saddas1rambler@gmail.com>
2025-06-16 22:26:44 +0300

    fix(player): register AchievementsRepository in providers

--- server/src/Services/PlayerService.Nodejs/src/modules/achievements/achievements.module.ts
+++ server/src/Services/PlayerService.Nodejs/src/modules/achievements/achievements.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AchievementsService } from './achievements.service';
 import { AchievementsController } from './achievements.controller';
 import { DatabaseModule } from '../../database/database.module';
+import { AchievementsRepository } from './achievements.repository';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [AchievementsController],
-  providers: [AchievementsService],
+  providers: [AchievementsService, AchievementsRepository],
   exports: [AchievementsService],
 })
 export class AchievementsModule {}
